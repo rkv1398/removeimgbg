8fbb99528cc6158284ae5fd076101d161c905550
Rishikesh <rishikesh.mmf@gmail.com>
2024-04-20 16:03:21 +0530

    added white remove bg functionality

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import rembg
-from PIL import Image
+from PIL import Image, ImageOps
 
 def remove_white_background(input_image_path, output_image_path):
     with open(input_image_path, "rb") as input_file:
@@ -8,11 +8,18 @@ def remove_white_background(input_image_path, output_image_path):
     with open(output_image_path, "wb") as output_file:
         output_file.write(input_image)
 
+def add_white_background(input_image_path, output_image_path):
+    processed_image = Image.open(input_image_path)
+    bg = Image.new('RGB', processed_image.size, (255, 255, 255))
+    bg.paste(processed_image, (0, 0), processed_image)
+    bg.save(output_image_path)
+
 input_image_path = r"images/image.jpg"
 output_image_path = r"images/output_image.png"
 
 remove_white_background(input_image_path, output_image_path)
+add_white_background(output_image_path, output_image_path + "_with_bg.png")
 
 # Open the processed image with PIL
-processed_image = Image.open(output_image_path)
+processed_image = Image.open(output_image_path + "_with_bg.png")
 processed_image.show()
